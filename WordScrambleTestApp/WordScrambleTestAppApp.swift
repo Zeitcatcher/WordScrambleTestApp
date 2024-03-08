3230aaf55659d84fb4009469f465dfcb4683cce9
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-03-07 18:22:32 -0800

    Initial Commit

---
+++ WordScrambleTestApp/WordScrambleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTestAppApp.swift
+//  WordScrambleTestApp
+//
+//  Created by Arseniy Oksenoyt on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
